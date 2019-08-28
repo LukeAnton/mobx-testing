4c3f3351a51ebdde884a2cb1d83a8c8fed019bec
Luke Firth <lukeantonfirth@gmail.com>
2019-08-29 09:50:22 +1000

    Observe Inject Calulated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { inject, observer } from "mobx-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// Gives access to the BirdStore passed in the Provider './index.js'
+
+const App = inject("BirdStore")(
+  observer(({ BirdStore }) => {
+    const { birds, birdCount, addBird } = BirdStore;
+
+    const handleSubmit = e => {
+      e.preventDefault();
+      const bird = BirdStore.bird.value;
+      addBird(bird);
+      BirdStore.bird.value = "";
+    };
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          Your State
+          {birds.length === 0 ? (
+            <h1>{birdCount}</h1>
+          ) : (
+            <h1>
+              {birds.map((bird, index) => (
+                <ul key={index}>
+                  {bird}
+                  {index}
+                </ul>
+              ))}
+            </h1>
+          )}
+          {birds}
+        </header>
+        <form onSubmit={e => handleSubmit(e)}>
+          <input
+            type="text"
+            placeholder="Enter State"
+            ref={input => (BirdStore.bird = input)}
+          />
+          <button>Add State</button>
+        </form>
+      </div>
+    );
+  }),
+);
 
 export default App;
